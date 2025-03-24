33b5c7127cf38ca03e868a70b4e26363c4344e92
HarryTheFeck <mihaow.myers@gmail.com>
2025-03-24 11:03:20 +0000

    test

--- suffering/suffering/settings.py
+++ suffering/suffering/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-gr(j-+zqzba!64aya&xnk2!hzxf3$b2vrj24g+=2!cxaqt6k-r'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = True
 
 ALLOWED_HOSTS = []
