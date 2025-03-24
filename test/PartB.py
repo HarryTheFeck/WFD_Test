5decd938ec3e113467327c5757e904563fd418bf
HarryTheFeck <mihaow.myers@gmail.com>
2025-03-24 15:32:23 +0000

    test

--- test/PartB.py
+++ test/PartB.py
@@ -19,10 +19,18 @@ class TestHouse(unittest.TestCase):
     def test_no_instance_of_house(self):
         self.assertNotIsInstance(self.house, RichHouse)
 
-    def test_no_instance_of_house(self):
+    def test_no_instance_of_richhouse(self):
         self.assertNotIsInstance(self.richhouse, House)
 
 #B4
 
+    def test_objects_are_not_identical(self):
+        house2 = House(45, "Talbot Street", "Dublin 8", 2, 300000)
+        self.assertIsNot(self.house, house2)
+    
+    def test_objects_are_identical(self):
+        same_house = self.house
+        self.assertIs(self.house, same_house)
 
+#B5
 
